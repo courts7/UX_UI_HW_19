6db50b419b927475ad3b2e24c562c54654f15e52
courts7 <92000012+courts7@users.noreply.github.com>
2022-04-14 08:45:59 -0600

    Ready to turn in HW

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 console.log ("Hello World");
 
 let myLogo = document.getElementById('myLogo');
-document.getElementById('myButton').onclick = function(){
+document.getElementById('myButton1').onclick = function(){
     myLogo.classlist('fade');
 
 }
 document.getElementById('myButton').addEventListener('click', myFunction);
 
 function myFunction() {
-    alert ("404 Error will be fix shortly");
+    alert ("404 Error Projects page coming soon");
+
+if (this.hash !== "") {
+    
+    event.preventDefault();
+var hash = this.hash;
+$('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function() {
+    window.location.hash = hash;
+});
+}
+  
 }
